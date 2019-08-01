249cd776037d0f8e627deaa1cf745be0f430e943
admin <siwei3636@163.com>
2019-08-01 09:15:37 +0800

    chengxuyuantgijiao

--- src/main/java/cn/itcast/GitTest.java
+++ src/main/java/cn/itcast/GitTest.java
@@ -2,6 +2,8 @@ package cn.itcast;
 
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("初始化控制");
+        System.out.println("chengxuyuan tuozhu");
     }
 }
